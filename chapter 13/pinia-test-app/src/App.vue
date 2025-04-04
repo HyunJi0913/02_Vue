feea9a048caecf7e5a4939c0da56306f0a9ee573
hyunji <dlakgjl55@naver.com>
2025-04-04 15:13:30 +0900

    20250404 심화 과제

---
+++ chapter 13/pinia-test-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { computed } from 'vue';
+import { useCounterStore } from './stores/counter';
+const store = useCounterStore();
+const { increment } = store; //분해할당
+const count = computed(() => store.count);
+
+increment();
+
+console.log;
+</script>
+
+<template>{{ store.count }}//{{ store.doubleCount }}<br />{{ count }}</template>
